0032aea162c922ada52e6b5cf09c5edaa01093ec
ethelegend <michel.elijah.k@gmail.com>
2024-04-05 12:37:21 +1300

    pls let this work

--- src/main/java/init/Initialiser.java
+++ src/main/java/init/Initialiser.java
@@ -1,7 +1,28 @@
 package init;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import main.*;
+
+import java.io.FileReader;
+import java.io.IOException;
 public class Initialiser {
-    public static void main(String[] args) {
-        new JSONReader();
+    public static void main(String args[]) {
+        JSONParser parser = new JSONParser();
+        JSONArray rooms = new JSONArray();
+        try {
+            rooms = (JSONArray) ((JSONObject) parser.parse(new FileReader("src/main/resources/rooms.json"))).get("rooms");
+        }catch (IOException e) {
+            System.err.println("Unable to find level data");
+            System.exit(1);
+        } catch (ParseException e) {
+            System.err.println("Unable to parse level data");
+            System.exit(2);
+        }
+        new Main(rooms);
+
     }
+
 }
