2ea3da9b653dcf692b84c5604630fd138e829758
ethelegend <michel.elijah.k@gmail.com>
2024-04-10 12:19:33 +1200

    more stuffs

--- src/main/java/monsters/Entity.java
+++ src/main/java/monsters/Entity.java
@@ -12,13 +12,29 @@ public class Entity {
     int maxHP;
     Weapon[] weapons;
     int pos;
+    boolean player;
     public String Attack(JFrame window, Entity target, int roomRadius) {
+        if (player) {
+            return this.ManualAttack(window, target, roomRadius);
+        } else {
+            return this.AutomaticAttack(window, target, roomRadius);
+        }
+
+    }
+    public String ManualAttack(JFrame window, Entity target, int roomRadius) {
+        return null;
+    }
+    public String AutomaticAttack(JFrame window, Entity target, int roomRadius) {
         /*  Explanation:
             The enemies are programmed to advance towards their target, and retreat when under half health.
             The other parts of the code is about if they are close to a wall or their target, in which case they should be stopped.
         */
         int right = (this.pos > target.pos) ? 1 : -1;
-        this.pos = (maxHP/hp < 2) // If you are not under half health
+        int movementTarget = (this.maxHP/this.hp < 2)
+                ? target.pos + 5 * right
+                : roomRadius * right;
+        right = (movementTarget > this.pos) ? 1 : -1;
+        this.pos = (maxHP/hp < 2) // If you are over half health
                 ? (Math.abs(this.pos - target.pos) > this.speed + 5) // If you are not within movement range of the enemy
                         ? this.pos - this.speed * right // Move towards the target
                         : target.pos + 5 * right // Move next to the target
@@ -33,5 +49,9 @@ public class Entity {
         }
 
         return null;
+    }
+
+    public int InfoPopup(JFrame window, String labelText, String[] buttonText) {
+        return 0;
     }
 }
